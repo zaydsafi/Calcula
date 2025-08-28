786792846bf59f6a3cb9cc265ab07baf4f4cdaa2
Salahuddin Safi <142176470+zaydsafi@users.noreply.github.com>
2025-08-28 20:49:43 +0430

    finnal changing

--- src/Pages/Calculator.jsx
+++ src/Pages/Calculator.jsx
@@ -7,37 +7,40 @@ const Calculator = () => {
     <div className="container">
       <div className="calculator">
         <form action="">
+          <div className="loos">
+            <input type="text" disabled />
+          </div>
           <div className="display">
             <input type="text" name="" id="" value={value} />
           </div>
           <div>
-            <input type="button" value="AC" onClick={e => setValue('')}  />
-            <input type="button" value="DE" onClick={e => setValue(value.slice(0, -1))}  />
-            <input type="button" value="." onClick={e => setValue(value + e.target.value)}  />
-            <input type="button" value="/" onClick={e => setValue(value + e.target.value)}  />
+            <input type="button" className="changecolor" value="AC" onClick={e => setValue('')}  />
+            <input type="button" className="changecolor" value="DE" onClick={e => setValue(value.slice(0, -1))}  />
+            <input type="button" className="changecolor" value="." onClick={e => setValue(value + e.target.value)}  />
+            <input type="button" className="changecolor" value="/" onClick={e => setValue(value + e.target.value)}  />
           </div>
           <div>
             <input type="button" value="7" onClick={e => setValue(value + e.target.value)} />
             <input type="button" value="8" onClick={e => setValue(value + e.target.value)}  />
             <input type="button" value="9" onClick={e => setValue(value + e.target.value)}  />
-            <input type="button" value="*" onClick={e => setValue(value + e.target.value)}  />
+            <input type="button" className="changecolor" value="*" onClick={e => setValue(value + e.target.value)}  />
           </div>
           <div>
             <input type="button" value="4" onClick={e => setValue(value + e.target.value)}  />
             <input type="button" value="5" onClick={e => setValue(value + e.target.value)}  />
             <input type="button" value="6" onClick={e => setValue(value + e.target.value)}  />
-            <input type="button" value="+" onClick={e => setValue(value + e.target.value)}  />
+            <input type="button" className="changecolor" value="+" onClick={e => setValue(value + e.target.value)}  />
           </div>
           <div>
             <input type="button" value="1" onClick={e => setValue(value + e.target.value)}  />
             <input type="button" value="2" onClick={e => setValue(value + e.target.value)}  />
             <input type="button" value="3" onClick={e => setValue(value + e.target.value)}  />
-            <input type="button" value="-" onClick={e => setValue(value + e.target.value)}  />
+            <input type="button" className="changecolor" value="-" onClick={e => setValue(value + e.target.value)}  />
           </div>
           <div>
             <input type="button" value="00" onClick={e => setValue(value + e.target.value)}  />
             <input type="button" value="0" onClick={e => setValue(value + e.target.value)}  />
-            <input type="button" value="=" onClick={e => setValue(eval(value))}  className="equal" />
+            <input type="button" value="=" onClick={e => setValue(eval(value))}  className="equal equalcolor" />
           </div>
         </form>
       </div>
